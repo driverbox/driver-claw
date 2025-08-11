e72b1ecd3292df90342d46a9ff20959e462c18fd
markmybytes <markmybytes@gmail.com>
2025-08-11 15:35:49 +0800

    Add `silent` option

--- src/archive.py
+++ src/archive.py
@@ -10,6 +10,6 @@ def unzip(source: os.PathLike, target: os.PathLike, silent: bool = True) -> int:
                      f"{"> nul" if silent else ""}")
 
 
-def zip(target: os.PathLike, *source: os.PathLike, level: int = 5, silent: bool = False) -> int:
+def zip(target: os.PathLike, *source: os.PathLike, level: int = 5, silent: bool = True) -> int:
     return os.system(f"{LIB7ZIP[:2]}\"{LIB7ZIP[2:]}\" a \"{target}\" {" ".join(source)} "
                      f"-mx{level} {"> nul" if silent else ""}")
--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 import argparse
 import os
 import shutil
+from contextlib import contextmanager, redirect_stdout
 
 import archive
 import config
 from driver_scraper import DriverScraper
+
+
+@contextmanager
+def setup_print(silent: bool):
+    if silent:
+        with open(os.devnull, 'w') as f, redirect_stdout(f):
+            yield
+    else:
+        yield
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -52,6 +63,12 @@ if __name__ == '__main__':
         action='extend',
         help='File or directory name to include in the archive'
     )
+    parser.add_argument(
+        '-s',
+        '--silent',
+        action='store_true',
+        help='Suppress all output messages'
+    )
 
     group_archive = parser.add_mutually_exclusive_group()
     group_archive.add_argument(
@@ -69,34 +86,36 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if not args.archive_only:
-        if not args.retry_failed and os.path.exists(args.output_dir):
-            shutil.rmtree(args.output_dir)
+    with setup_print(args.silent):
+        if not args.archive_only:
+            if not args.retry_failed and os.path.exists(args.output_dir):
+                shutil.rmtree(args.output_dir)
 
-        try:
-            scraper = DriverScraper(args.output_dir)
-            failed = scraper.scrape(scraper.load_failed() if args.retry_failed else config.BASE_CONFIG,
-                                    args.error_handling)
-        except FileNotFoundError:
-            print('Nothing to retry.')
+            try:
+                scraper = DriverScraper(args.output_dir)
+                failed = scraper.scrape(scraper.load_failed() if args.retry_failed else config.BASE_CONFIG,
+                                        args.error_handling)
+            except FileNotFoundError:
+                print('Nothing to retry.')
+                exit(1)
+
+            if len(failed) > 0:
+                print(f'Total of {len(failed)} download(s) failed.')
+                print('You may use -r/--retry-failed option to try again.')
+                exit(1)
+            if args.no_archive:
+                exit(0)
+
+        if not os.path.exists(args.output_dir):
+            print(
+                f"Error: Output directory '{args.output_dir}' does not exist.")
+            exit(1)
+        if not os.listdir(args.output_dir):
+            print(f"Error: Output directory '{args.output_dir}' is empty.")
             exit(1)
 
-        if len(failed) > 0:
-            print(f'Total of {len(failed)} download(s) failed.')
-            print('You may use -r/--retry-failed option to try again.')
-            exit(1)
-        if args.no_archive:
-            exit(0)
-
-    if not os.path.exists(args.output_dir):
-        print(
-            f"Error: Output directory '{args.output_dir}' does not exist.")
-        exit(1)
-    if not os.listdir(args.output_dir):
-        print(f"Error: Output directory '{args.output_dir}' is empty.")
-        exit(1)
-
-    archive.zip(args.archive_name,
-                *(args.include_files or []),
-                args.output_dir,
-                level=args.compress_level)
+        archive.zip(args.archive_name,
+                    *(args.include_files or []),
+                    args.output_dir,
+                    level=args.compress_level,
+                    silent=args.silent)
