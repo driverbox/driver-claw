525de7555c24a97475363e04de7fe62b710181d9
markmybytes <markmybytes@gmail.com>
2025-08-11 15:56:54 +0800

    Uncomment configs

--- src/config.py
+++ src/config.py
@@ -8,171 +8,171 @@ BASE_CONFIG: dict[
     Literal['display', 'network', 'miscellaneous', 'tool'],
     list[ScrapeItem]
 ] = {
-    # 'display': [
-    #     {
-    #         'path': 'AMD',
-    #         'url': functools.partial(
-    #             url.amd,
-    #             url='https://www.amd.com/en/support/downloads/drivers.html/graphics/radeon-rx/radeon-rx-9000-series/amd-radeon-rx-9070-xt.html',
-    #             keyword='win10-win11'),
-    #         'file_type': 'zip',
-    #         'rename_as': None
-    #     },
-    #     {
-    #         'path': 'Intel® 7th-10th Gen Processor Graphics',
-    #         'url': functools.partial(
-    #             url.intel,
-    #             url='https://www.intel.com/content/www/us/en/download/776137/intel-7th-10th-gen-processor-graphics-windows.html'
-    #         ),
-    #         'file_type': 'zip',
-    #         'rename_as': None
-    #     },
-    #     {
-    #         'path': 'Intel® Arc™ & Iris® Xe Graphics',
-    #         'url': functools.partial(
-    #             url.intel,
-    #             url='https://www.intel.com/content/www/us/en/download/785597/intel-arc-iris-xe-graphics-windows.html'
-    #         ),
-    #         'file_type': 'zip',
-    #         'rename_as': None
-    #     },
-    #     {
-    #         'path': 'Nvidia',
-    #         'url': functools.partial(url.nvidia_grd, type='desktop'),
-    #         'file_type': 'zip',
-    #         'rename_as': None
-    #     }
-    # ],
+    'display': [
+        {
+            'path': 'AMD',
+            'url': functools.partial(
+                    url.amd,
+                    url='https://www.amd.com/en/support/downloads/drivers.html/graphics/radeon-rx/radeon-rx-9000-series/amd-radeon-rx-9070-xt.html',
+                    keyword='win10-win11'),
+            'file_type': 'zip',
+            'rename_as': None
+        },
+        {
+            'path': 'Intel® 7th-10th Gen Processor Graphics',
+            'url': functools.partial(
+                url.intel,
+                url='https://www.intel.com/content/www/us/en/download/776137/intel-7th-10th-gen-processor-graphics-windows.html'
+            ),
+            'file_type': 'zip',
+            'rename_as': None
+        },
+        {
+            'path': 'Intel® Arc™ & Iris® Xe Graphics',
+            'url': functools.partial(
+                url.intel,
+                url='https://www.intel.com/content/www/us/en/download/785597/intel-arc-iris-xe-graphics-windows.html'
+            ),
+            'file_type': 'zip',
+            'rename_as': None
+        },
+        {
+            'path': 'Nvidia',
+            'url': functools.partial(url.nvidia_grd, type='desktop'),
+            'file_type': 'zip',
+            'rename_as': None
+        }
+    ],
     'miscellaneous': [
-        # {
-        #     'path': 'AMD Chipset',
-        #     'url': functools.partial(
-        #         url.amd,
-        #         url='https://www.amd.com/en/support/downloads/drivers.html/chipsets/am5/x870e.html',
-        #         keyword='chipset'),
-        #     'file_type': 'exe',
-        #     'rename_as': 'AMD_Chipset_Software'
-        # },
-        # {
-        #     'path': 'Intel Chipset (INF Utility)',
-        #     'url': functools.partial(
-        #         url.intel,
-        #         url='https://www.intel.com/content/www/us/en/download/19347/chipset-inf-utility.html'
-        #     ),
-        #     'file_type': 'exe',
-        #     'rename_as': None
-        # },
-        # {
-        #     'path': 'Intel® PPM',
-        #     'url': functools.partial(
-        #         url.gigabyte,
-        #         url='https://www.gigabyte.com/Motherboard/B860M-AORUS-ELITE-WIFI6E/support#support-dl-driver-wlanbt',
-        #         dri_name='Platform Power Management(PPM)'
-        #     ),
-        #     'file_type': 'zip/exe',
-        #     'rename_as': 'mb_driver_3713_ppm'
-        # },
-        # {
-        #     'path': 'Intel® Wireless',
-        #     'url': functools.partial(
-        #         url.intel,
-        #         url='https://www.intel.com.tw/content/www/us/en/download/19351/intel-wireless-wi-fi-drivers-for-windows-10-and-windows-11.html'
-        #     ),
-        #     'file_type': 'exe',
-        #     'rename_as': 'WiFi-Driver64-Win10-Win11'
-        # },
-        # {
-        #     'path': 'Intel® Wireless',
-        #     'url': functools.partial(
-        #         url.intel,
-        #         url='https://www.intel.com.tw/content/www/us/en/download/18649/intel-wireless-bluetooth-drivers-for-windows-10-and-windows-11.html'
-        #     ),
-        #     'file_type': 'exe',
-        #     'rename_as': 'BT-UWD-Win10-Win11'
-        # },
-        # {
-        #     'path': 'MediaTek MT7961_79X2\\Bluetooth',
-        #     'url': functools.partial(
-        #         url.gigabyte,
-        #         url='https://www.gigabyte.com/hk/Motherboard/B850M-FORCE-WIFI6E/support#dl',
-        #         dri_name='MediaTek Wi-Fi 6E Bluetooth Driver'
-        #     ),
-        #     'file_type': 'zip/exe',
-        #     'rename_as': 'mb_driver_4717_mtk6e'
-        # },
-        # {
-        #     'path': 'MediaTek MT7961_79X2\\WIFI',
-        #     'url': functools.partial(
-        #         url.gigabyte,
-        #         url='https://www.gigabyte.com/hk/Motherboard/B850M-FORCE-WIFI6E/support#dl',
-        #         dri_name='MediaTek Wi-Fi 6E WIFI Driver'
-        #     ),
-        #     'file_type': 'zip/exe',
-        #     'rename_as': 'mb_driver_4716_mtk6ewifi'
-        # },
-        # {
-        #     'path': 'MediaTek MT7952_7927\\Bluetooth',
-        #     'url': functools.partial(
-        #         url.gigabyte_wifi_card,
-        #         version='GC-WIFI7 1.1',
-        #         dri_name='Bluetooth'
-        #     ),
-        #     'file_type': 'zip/exe',
-        #     'rename_as': 'mb_driver_2683_mtk'
-        # },
-        # {
-        #     'path': 'MediaTek MT7952_7927\\WIFI',
-        #     'url': functools.partial(
-        #         url.gigabyte_wifi_card,
-        #         version='GC-WIFI7 1.1',
-        #         dri_name='WIFI'
-        #     ),
-        #     'file_type': 'zip/exe',
-        #     'rename_as': 'mb_driver_2682_mtk'
-        # },
-        # {
-        #     'path': 'Qualcomm NCM865\\Bluetooth',
-        #     'url': functools.partial(
-        #         url.gigabyte_wifi_card,
-        #         version='GC-WIFI7 1.0',
-        #         dri_name='Bluetooth'
-        #     ),
-        #     'file_type': 'zip/exe',
-        #     'rename_as': 'mb_driver_2687_qualcomm'
-        # },
-        # {
-        #     'path': 'Qualcomm NCM865\\WIFI',
-        #     'url': functools.partial(
-        #         url.gigabyte_wifi_card,
-        #         version='GC-WIFI7 1.0',
-        #         dri_name='WIFI'
-        #     ),
-        #     'file_type': 'zip/exe',
-        #     'rename_as': 'mb_driver_2686_qualcomm'
-        # },
-        # {
-        #     'path': 'Realtek HD Universal',
-        #     'url': functools.partial(
-        #         url.msi,
-        #         url='https://hk.msi.com/Motherboard/MAG-X870-TOMAHAWK-WIFI/support#driver',
-        #         dri_type='On-Board Audio Drivers',
-        #         dri_name='Realtek HD Universal Driver'
-        #     ),
-        #     'file_type': 'zip/folder',
-        #     'rename_as': None
-        # },
-        # {
-        #     'path': 'Realtek RTL8852BE\\Bluetooth',
-        #     'url': 'https://dlcdnets.asus.com/pub/ASUS/mb/02BT/DRV_BT_RTK_8852BE_SZ-TSD_W11_64_V1640132401503_20240924R.zip?model=PRIME%20B650M-A%20WIFI',
-        #     'file_type': 'zip',
-        #     'rename_as': None
-        # },
-        # {
-        #     'path': 'Realtek RTL8852BE\\WIFI',
-        #     'url': 'https://dlcdnets.asus.com/pub/ASUS/mb/08WIRELESS/DRV_WiFi_RTK_8852BE_SZ-TSD_W11_64_V6001151240_20220908B.zip?model=PRIME%20B650M-A%20WIFI',
-        #     'file_type': 'zip',
-        #     'rename_as': None
-        # },
+        {
+            'path': 'AMD Chipset',
+            'url': functools.partial(
+                url.amd,
+                url='https://www.amd.com/en/support/downloads/drivers.html/chipsets/am5/x870e.html',
+                keyword='chipset'),
+            'file_type': 'exe',
+            'rename_as': 'AMD_Chipset_Software'
+        },
+        {
+            'path': 'Intel Chipset (INF Utility)',
+            'url': functools.partial(
+                url.intel,
+                url='https://www.intel.com/content/www/us/en/download/19347/chipset-inf-utility.html'
+            ),
+            'file_type': 'exe',
+            'rename_as': None
+        },
+        {
+            'path': 'Intel® PPM',
+            'url': functools.partial(
+                url.gigabyte,
+                url='https://www.gigabyte.com/Motherboard/B860M-AORUS-ELITE-WIFI6E/support#support-dl-driver-wlanbt',
+                dri_name='Platform Power Management(PPM)'
+            ),
+            'file_type': 'zip/exe',
+            'rename_as': 'mb_driver_3713_ppm'
+        },
+        {
+            'path': 'Intel® Wireless',
+            'url': functools.partial(
+                url.intel,
+                url='https://www.intel.com.tw/content/www/us/en/download/19351/intel-wireless-wi-fi-drivers-for-windows-10-and-windows-11.html'
+            ),
+            'file_type': 'exe',
+            'rename_as': 'WiFi-Driver64-Win10-Win11'
+        },
+        {
+            'path': 'Intel® Wireless',
+            'url': functools.partial(
+                url.intel,
+                url='https://www.intel.com.tw/content/www/us/en/download/18649/intel-wireless-bluetooth-drivers-for-windows-10-and-windows-11.html'
+            ),
+            'file_type': 'exe',
+            'rename_as': 'BT-UWD-Win10-Win11'
+        },
+        {
+            'path': 'MediaTek MT7961_79X2\\Bluetooth',
+            'url': functools.partial(
+                url.gigabyte,
+                url='https://www.gigabyte.com/hk/Motherboard/B850M-FORCE-WIFI6E/support#dl',
+                dri_name='MediaTek Wi-Fi 6E Bluetooth Driver'
+            ),
+            'file_type': 'zip/exe',
+            'rename_as': 'mb_driver_4717_mtk6e'
+        },
+        {
+            'path': 'MediaTek MT7961_79X2\\WIFI',
+            'url': functools.partial(
+                url.gigabyte,
+                url='https://www.gigabyte.com/hk/Motherboard/B850M-FORCE-WIFI6E/support#dl',
+                dri_name='MediaTek Wi-Fi 6E WIFI Driver'
+            ),
+            'file_type': 'zip/exe',
+            'rename_as': 'mb_driver_4716_mtk6ewifi'
+        },
+        {
+            'path': 'MediaTek MT7952_7927\\Bluetooth',
+            'url': functools.partial(
+                url.gigabyte_wifi_card,
+                version='GC-WIFI7 1.1',
+                dri_name='Bluetooth'
+            ),
+            'file_type': 'zip/exe',
+            'rename_as': 'mb_driver_2683_mtk'
+        },
+        {
+            'path': 'MediaTek MT7952_7927\\WIFI',
+            'url': functools.partial(
+                url.gigabyte_wifi_card,
+                version='GC-WIFI7 1.1',
+                dri_name='WIFI'
+            ),
+            'file_type': 'zip/exe',
+            'rename_as': 'mb_driver_2682_mtk'
+        },
+        {
+            'path': 'Qualcomm NCM865\\Bluetooth',
+            'url': functools.partial(
+                url.gigabyte_wifi_card,
+                version='GC-WIFI7 1.0',
+                dri_name='Bluetooth'
+            ),
+            'file_type': 'zip/exe',
+            'rename_as': 'mb_driver_2687_qualcomm'
+        },
+        {
+            'path': 'Qualcomm NCM865\\WIFI',
+            'url': functools.partial(
+                url.gigabyte_wifi_card,
+                version='GC-WIFI7 1.0',
+                dri_name='WIFI'
+            ),
+            'file_type': 'zip/exe',
+            'rename_as': 'mb_driver_2686_qualcomm'
+        },
+        {
+            'path': 'Realtek HD Universal',
+            'url': functools.partial(
+                url.msi,
+                url='https://hk.msi.com/Motherboard/MAG-X870-TOMAHAWK-WIFI/support#driver',
+                dri_type='On-Board Audio Drivers',
+                dri_name='Realtek HD Universal Driver'
+            ),
+            'file_type': 'zip/folder',
+            'rename_as': None
+        },
+        {
+            'path': 'Realtek RTL8852BE\\Bluetooth',
+            'url': 'https://dlcdnets.asus.com/pub/ASUS/mb/02BT/DRV_BT_RTK_8852BE_SZ-TSD_W11_64_V1640132401503_20240924R.zip?model=PRIME%20B650M-A%20WIFI',
+            'file_type': 'zip',
+            'rename_as': None
+        },
+        {
+            'path': 'Realtek RTL8852BE\\WIFI',
+            'url': 'https://dlcdnets.asus.com/pub/ASUS/mb/08WIRELESS/DRV_WiFi_RTK_8852BE_SZ-TSD_W11_64_V6001151240_20220908B.zip?model=PRIME%20B650M-A%20WIFI',
+            'file_type': 'zip',
+            'rename_as': None
+        },
         {
             'path': 'Realtek RTL8852CE\\Bluetooth',
             'url': functools.partial(
@@ -214,65 +214,65 @@ BASE_CONFIG: dict[
             'rename_as': 'mb_driver_3701_realtek8922wifi'
         }
     ],
-    # 'network': [
-    #     {
-    #         'path': 'Intel® Ethernet Adapter Complete Driver Pack',
-    #         'url': functools.partial(
-    #             url.intel,
-    #             url='https://www.intel.com/content/www/us/en/download/15084/intel-ethernet-adapter-complete-driver-pack.html'
-    #         ),
-    #         'file_type': 'zip',
-    #         'rename_as': None
-    #     },
-    #     {
-    #         'path': 'Realtek',
-    #         'url': functools.partial(
-    #             url.msi,
-    #             url='https://hk.msi.com/Motherboard/MAG-X870-TOMAHAWK-WIFI/support#driver',
-    #             dri_type='LAN Drivers',
-    #             dri_name='Realtek PCI-E Ethernet Drivers'
-    #         ),
-    #         'file_type': 'zip/folder',
-    #         'rename_as': None
-    #     }
-    # ],
-    # 'tool': [
-    #     {
-    #         'path': 'CrystalDiskinfo',
-    #         'url': url.crystaldick_info,
-    #         'file_type': 'exe',
-    #         'rename_as': None
+    'network': [
+        {
+            'path': 'Intel® Ethernet Adapter Complete Driver Pack',
+            'url': functools.partial(
+                url.intel,
+                url='https://www.intel.com/content/www/us/en/download/15084/intel-ethernet-adapter-complete-driver-pack.html'
+            ),
+            'file_type': 'zip',
+            'rename_as': None
+        },
+        {
+            'path': 'Realtek',
+            'url': functools.partial(
+                url.msi,
+                url='https://hk.msi.com/Motherboard/MAG-X870-TOMAHAWK-WIFI/support#driver',
+                dri_type='LAN Drivers',
+                dri_name='Realtek PCI-E Ethernet Drivers'
+            ),
+            'file_type': 'zip/folder',
+            'rename_as': None
+        }
+    ],
+    'tool': [
+        {
+            'path': 'CrystalDiskinfo',
+            'url': url.crystaldick_info,
+            'file_type': 'exe',
+            'rename_as': None
 
-    #     },
-    #     {
-    #         'path': 'CrystalDiskMark',
-    #         'url': url.crystaldick_mark,
-    #         'file_type': 'zip/exe',
-    #         'rename_as': None
-    #     },
-    #     {
-    #         'path': 'FurMark',
-    #         'url': url.furmark,
-    #         'file_type': 'zip/folder',
-    #         'rename_as': None
-    #     },
-    #     {
-    #         'path': 'HWInfo',
-    #         'url': url.hwinfo,
-    #         'file_type': 'zip/exe',
-    #         'rename_as': None
-    #     },
-    #     {
-    #         'path': 'OCCT',
-    #         'url': url.occt,
-    #         'file_type': 'exe',
-    #         'rename_as': None
-    #     },
-    #     {
-    #         'path': 'y-cruncher',
-    #         'url': url.y_cruncher,
-    #         'file_type': 'zip/folder',
-    #         'rename_as': None
-    #     }
-    # ]
+        },
+        {
+            'path': 'CrystalDiskMark',
+            'url': url.crystaldick_mark,
+            'file_type': 'zip/exe',
+            'rename_as': None
+        },
+        {
+            'path': 'FurMark',
+            'url': url.furmark,
+            'file_type': 'zip/folder',
+            'rename_as': None
+        },
+        {
+            'path': 'HWInfo',
+            'url': url.hwinfo,
+            'file_type': 'zip/exe',
+            'rename_as': None
+        },
+        {
+            'path': 'OCCT',
+            'url': url.occt,
+            'file_type': 'exe',
+            'rename_as': None
+        },
+        {
+            'path': 'y-cruncher',
+            'url': url.y_cruncher,
+            'file_type': 'zip/folder',
+            'rename_as': None
+        }
+    ]
 }
